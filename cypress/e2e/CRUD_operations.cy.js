47cce895d185ea4a5c6603d3d4728f111e3df7c5
Eduardo Lopez <eduardooscar.lopez@globant.com>
2023-05-10 03:06:42 -0300

    api plugin setup

--- cypress/e2e/CRUD_operations.cy.js
+++ cypress/e2e/CRUD_operations.cy.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
 describe('CRUD operations', () => {
-  it('GET API testing Using Cypress ', () => {
-    cy.request('GET', 'https://reqres.in/api/users?page=2').should((response) => {
+  it('GET API testing Using Cypress API Plugin', () => {
+    cy.api('GET', 'https://reqres.in/api/users?page=2').should((response) => {
       expect(response.status).to.eq(200)
     })
   })
-  it('POST API testing Using Cypress ', () => {
-    cy.request('POST', 'https://reqres.in/api/users', {
+  it('POST API testing Using Cypress API Plugin', () => {
+    cy.api('POST', 'https://reqres.in/api/users', {
       name: 'Eduardo Lopez',
       job: 'QA Automation Tester'
     }).should((response) => {
       expect(response.status).to.eq(201)
     })
   })
-  it('PUT API testing Using Cypress', () => {
-    cy.request('PUT', 'https://reqres.in/api/users/2', {
-      name: 'QAAutomationLabs',
-      job: 'QA Automation Engg'
+  it('PUT API testing Using Cypress API Plugin', () => {
+    cy.api('PUT', 'https://reqres.in/api/users/2', {
+      name: 'Vanesa ',
+      job: 'Graphic Designer'
     }).should((response) => {
       expect(response.status).to.eq(200)
     })
   })
-  it('DELETE API testing Using Cypress ', () => {
-    cy.request('DELETE', 'https://reqres.in/api/users/2').should((response) => {
+  it('DELETE API testing Using Cypress API Plugin', () => {
+    cy.api('DELETE', 'https://reqres.in/api/users/2').should((response) => {
       expect(response.status).to.eq(204)
     })
   })
