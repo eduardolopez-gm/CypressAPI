12217c2659ec2aab6c8bc7c5b95701eb042dbbd0
Eduardo Lopez <eduardooscar.lopez@globant.com>
2023-05-11 14:42:57 -0300

    fix mochawesome config

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents (on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on)
     }
   }
