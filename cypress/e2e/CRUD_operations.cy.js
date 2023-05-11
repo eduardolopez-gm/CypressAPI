aae262c394f2dafcaad28a20cab5a2ee39b0ff00
Eduardo Lopez <eduardooscar.lopez@globant.com>
2023-05-11 14:31:03 -0300

    remove unneccesary packages to leave as simple API tests

--- cypress/e2e/CRUD_operations.cy.js
+++ cypress/e2e/CRUD_operations.cy.js
@@ -2,12 +2,12 @@
 /// <reference types="Cypress" />
 describe('CRUD operations', () => {
   it('GET API testing Using Cypress API Plugin', () => {
-    cy.api('GET', 'https://reqres.in/api/users?page=2').should((response) => {
+    cy.request('GET', 'https://reqres.in/api/users?page=2').should((response) => {
       expect(response.status).to.eq(200)
     })
   })
   it('POST API testing Using Cypress API Plugin', () => {
-    cy.api('POST', 'https://reqres.in/api/users', {
+    cy.request('POST', 'https://reqres.in/api/users', {
       name: 'Eduardo Lopez',
       job: 'QA Automation Tester'
     }).should((response) => {
@@ -15,7 +15,7 @@ describe('CRUD operations', () => {
     })
   })
   it('PUT API testing Using Cypress API Plugin', () => {
-    cy.api('PUT', 'https://reqres.in/api/users/2', {
+    cy.request('PUT', 'https://reqres.in/api/users/2', {
       name: 'Vanesa ',
       job: 'Graphic Designer'
     }).should((response) => {
@@ -23,7 +23,7 @@ describe('CRUD operations', () => {
     })
   })
   it('DELETE API testing Using Cypress API Plugin', () => {
-    cy.api('DELETE', 'https://reqres.in/api/users/2').should((response) => {
+    cy.request('DELETE', 'https://reqres.in/api/users/2').should((response) => {
       expect(response.status).to.eq(204)
     })
   })
